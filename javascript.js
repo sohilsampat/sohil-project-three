33ac1e3ac90d34b1c9b791b46f447c9cef910505
Sohil <sohilsampat@gmail.com>
2020-05-07 15:14:42 -0500

    Move javascript to another file

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var x = document.getElementById("navigation-panel");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  }
+  else {
+    x.style.display = "block";
+  }
+}
